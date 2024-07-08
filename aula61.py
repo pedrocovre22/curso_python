aae03564844d095de76adc099fb3a5be0f3eb159
pedrocovre22 <pedro.j.covre@gmail.com>
2024-07-08 19:38:35 -0300

    python

---
+++ aula61.py
@@ -0,0 +1,9 @@
+# condicao = 10 == 11
+# var = 'valor' if condicao else 'outro valor'
+# print(var)
+
+# digito = 11
+#  novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+print('valor' if True else 'outro valor' if True else 'fim')

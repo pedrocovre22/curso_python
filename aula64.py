ce2e39f511217ba419d6c21694032bd54745f293
pedrocovre22 <pedro.j.covre@gmail.com>
2024-07-11 18:23:30 -0300

    usando def para repetir funções

---
+++ aula64.py
@@ -0,0 +1,18 @@
+# def Print(a, b, c):
+#     print('varias')
+#     print('varias')
+#     print('varias')
+#     print('varias')
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+
+def saudacao(nome = 'sem nome'):
+    print(f'ola,{nome}')
+
+saudacao('pedro')
+saudacao()
